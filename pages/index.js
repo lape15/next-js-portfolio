e3a6cc25b8f7226d5437f098e3ca2e06225e88a9
Lape15 <larape1993@gmail.com>
2020-09-17 12:17:54 +0100

    pushing to vercel

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ const Home = () => {
     AOS.init({
       duration: 1500,
     });
-    const interval = setTimeout(handleLoading, 42000);
+    const interval = setTimeout(handleLoading, 12000);
     return () => {
       clearInterval(interval);
     };
